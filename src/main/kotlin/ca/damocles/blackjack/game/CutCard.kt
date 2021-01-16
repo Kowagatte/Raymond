1e8a0295c95d4ebae615e8b37d1f80b0f68f12f2
Nick Ryan <nnryanp@gmail.com>
2021-01-15 21:43:55 -0600

    Create CutCard.kt
    Added cut card, so the game knows when to shuffle the shoe.

---
+++ src/main/kotlin/ca/damocles/blackjack/game/CutCard.kt
@@ -0,0 +1,7 @@
+package ca.damocles.blackjack.game
+
+import ca.damocles.bicycle.Card
+import ca.damocles.bicycle.Suit
+import ca.damocles.bicycle.Value
+
+class CutCard: Card(Suit.CUT, Value.CUT) {}

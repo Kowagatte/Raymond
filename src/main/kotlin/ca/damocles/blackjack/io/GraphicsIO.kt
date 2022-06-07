405bc34fd6347b48231681c28dfc0a9e727cea88
Nicholas Ryan <nnr635@usask.ca>
2022-06-06 22:23:05 -0600

    Having major JFrame Issues...

--- src/main/kotlin/ca/damocles/blackjack/io/GraphicsIO.kt
+++ src/main/kotlin/ca/damocles/blackjack/io/GraphicsIO.kt
@@ -1,49 +1,25 @@
 package ca.damocles.blackjack.io
 
-import ca.damocles.bicycle.Card
-import ca.damocles.bicycle.Suit
-import ca.damocles.bicycle.Value
 import java.awt.Graphics
-import java.awt.Image
-import javax.imageio.ImageIO
 import javax.swing.JFrame
-import javax.swing.JPanel
 import javax.swing.WindowConstants
 
 class GraphicsIO: IOForm {
 
-    var showing: Card = Card(Suit.SPADE, Value.ACE)
-    var message = "FlipThrough"
-    val frame: JFrame = object : JFrame("FlipThrough"){
+    val frame: JFrame = object : JFrame("Default Frame"){
         override fun paint(g: Graphics?) {
             super.paint(g)
-            g?.drawString(message, this.size.width/2, 100)
-            g?.drawImage(getImageFromCard(), this.size.width/2, this.size.height/2, null)
         }
     }
 
     init{
-
         frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         frame.setSize(800, 600)
         frame.isVisible = true
-
-    }
-
-    fun getImageFromCard(): Image{
-        println("${showing.suit.formattedName}/${showing.value.simpleRepresentation}.png")
-        return ImageIO.read(this.javaClass.getResource("/${showing.suit.formattedName}/${showing.value.simpleRepresentation}.png"));
     }
 
     override fun print(message: Any) {
-        if(message is Card){
-            showing = message
-            frame.repaint()
-        }
-        if(message is String){
-            this.message = message
-            frame.repaint()
-        }
+        //TODO
     }
 
     override fun input(): String {
--- src/main/kotlin/ca/damocles/trainer/flipthrough/FlipThrough.kt
+++ src/main/kotlin/ca/damocles/trainer/flipthrough/FlipThrough.kt
@@ -1,15 +1,84 @@
 package ca.damocles.trainer.flipthrough
 
+import ca.damocles.bicycle.Card
 import ca.damocles.bicycle.MultiDeck
-import ca.damocles.blackjack.io.GraphicsIO
-import ca.damocles.blackjack.io.IOAccess
+import ca.damocles.bicycle.Suit
+import ca.damocles.bicycle.Value
 import ca.damocles.blackjack.io.IOForm
+import java.awt.BorderLayout
+import java.awt.FlowLayout
+import java.awt.Graphics
+import java.awt.Image
 import java.lang.Thread.sleep
+import javax.imageio.ImageIO
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.JTextField
+import javax.swing.WindowConstants
 import kotlin.concurrent.thread
+
+
+class FlipThroughIO: IOForm{
+
+    private var showing: Card = Card(Suit.SPADE, Value.ACE)
+    var message = "FlipThrough"
+    var prompting = false
+    var inputBox = JTextField("1000")
+
+    private val frame: JFrame = JFrame("FlipThrough")
+
+    private val panel: JPanel = object : JPanel(){
+        override fun paint(g: Graphics?) {
+            super.paint(g)
+            g?.drawString(message, this.size.width/2 - message.length*3, 100)
+            val img = getImageFromCard()
+            g?.drawImage(img, this.size.width/2 - img.getWidth(null)/2, this.size.height/2 - img.getHeight(null)/2, null)
+        }
+    }
+
+    init{
+        frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+        frame.setSize(800, 600)
+        frame.isResizable = false
+
+
+        //inputBox.setLocation(frame.size.width/2, (frame.size.height/3) * 2)
+        //inputBox.setLocation(frame.size.width/2, (frame.size.height/3)*2)
+        //inputBox.setLocation(0, 0)
+        println("${frame.size.height}")
+
+        panel.add(inputBox)
+
+        frame.add(panel)
+        frame.isVisible = true
+
+    }
+
+    fun getImageFromCard(): Image {
+        //println("${showing.suit.formattedName}/${showing.value.simpleRepresentation}.png")
+        return ImageIO.read(this.javaClass.getResource("/${showing.suit.formattedName}/${showing.value.simpleRepresentation}.png"));
+    }
+
+    override fun print(message: Any) {
+        if(message is Card){
+            showing = message
+            frame.repaint()
+        }
+        if(message is String){
+            this.message = message
+            frame.repaint()
+        }
+    }
+
+    override fun input(): String {
+        return readLine().toString()
+    }
+
+}
 
 fun main(){
 
-    val io: IOForm = GraphicsIO()
+    val io: IOForm = FlipThroughIO()
 
     io.print("How many decks to flip through?")
     //val numOfDecks: Int = IOAccess.io.input().toInt()

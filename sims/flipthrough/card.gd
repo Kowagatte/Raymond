ce963045495e66ae32e88f6b066ceb13b8b55017
Nicholas Ryan <nnr635@usask.ca>
2022-06-07 21:34:17 -0600

    drawing cards to the screen complete

--- sims/flipthrough/card.gd
+++ sims/flipthrough/card.gd
@@ -5,8 +5,10 @@ class_name Card
 enum Suits {NONE, HEART, DIAMOND, CLUB, SPADE}
 enum Values {NONE, ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
 
+const hilo = [null, -1, 1, 1, 1, 1, 1, 0, 0, 0, -1, -1, -1, -1]
 const pv =  ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 const ps = ['', '♥', '♦', '♣', '♠']
+const suit_long_name = ['', 'Heart', 'Diamond', 'Club', 'Spade']
 
 var suit: int
 var value: int
@@ -14,6 +16,12 @@ var value: int
 func _init(starting_suit:int = Suits.NONE, starting_value:int = Values.NONE):
 	suit = starting_suit
 	value = starting_value
+	
+func getResourcePath():
+	return "res://res/"+str(suit_long_name[suit])+"/"+str(pv[value])+".png"
+
+func hilo_value():
+	return hilo[value]
 	
 func _to_string():
 	return "{" + str(ps[suit])+str(pv[value])+"}"
--- sims/flipthrough/flipthrough.gd
+++ sims/flipthrough/flipthrough.gd
@@ -1,16 +1,25 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum FTStates {TIMER_INPUT, FLIPPING, ASKING, RESPONSE}
 
+# Static after intialization 
+var timer_limit = 1000
+var card_sprite
+var timer
+var linedit
+
+# Dynamic values
+var state = FTStates.TIMER_INPUT
 var showing = Card.new(Card.Suits.SPADE, Card.Values.ACE)
-
 var deck = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	linedit = get_node("LineEdit") as LineEdit
+	card_sprite = get_node("card") as Sprite
+	linedit.connect("text_entered", self, "input")
 	
+	# Construct a deck of cards and randomly shuffle.
 	for x in range(1, Card.Suits.size()):
 		for y in range(1, Card.Values.size()):
 			deck.append(Card.new(x, y))
@@ -18,11 +27,39 @@ func _ready():
 	randomize()
 	deck.shuffle()
 	
+func input(inp):
+	if state == FTStates.TIMER_INPUT:
+		start_timer(float(inp))
+		state = FTStates.FLIPPING
 	
-	print(deck)
-	pass # Replace with function body.
+	if state == FTStates.ASKING:
+		pass
 
+func start_timer(delay):
+	timer = Timer.new()
+	timer.set_wait_time(delay)
+	timer.set_one_shot(false)
+	timer.connect("timeout", self, "flip")
+	add_child(timer)
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Takes a card off the top of the deck and shows it.
+func flip():
+	showing = deck.pop_front() as Card
+	var texture = load(showing.getResourcePath())
+	card_sprite.texture = texture
+	#print(deck)
+	if deck.empty():
+		timer.stop()
+
+var previous_state = null
+func _process(delta):
+	match(state):
+		FTStates.TIMER_INPUT:
+			pass
+		FTStates.FLIPPING:
+			pass
+		FTStates.ASKING:
+			pass
+		FTStates.RESPONSE:
+			pass

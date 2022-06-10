1bac9f6789d39fa680e69f0ba6aedbf598d0a826
Nicholas Ryan <nnr635@usask.ca>
2022-06-10 12:17:45 -0600

    Fully finished FlipThrough Sim

--- sims/flipthrough/card.gd
+++ sims/flipthrough/card.gd
@@ -2,13 +2,13 @@ extends Node
 
 class_name Card
 
-enum Suits {NONE, HEART, DIAMOND, CLUB, SPADE}
-enum Values {NONE, ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
+enum Suits {NONE, HEART, CLUB, DIAMOND, SPADE}
+enum Values {NONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
 
-const hilo = [null, -1, 1, 1, 1, 1, 1, 0, 0, 0, -1, -1, -1, -1]
-const pv =  ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-const ps = ['', '♥', '♦', '♣', '♠']
-const suit_long_name = ['', 'Heart', 'Diamond', 'Club', 'Spade']
+const hilo = [0, 1, 1, 1, 1, 1, 0, 0, 0, -1, -1, -1, -1, -1]
+const pv =  ['', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+const ps = ['', '♥', '♣', '♦', '♠']
+const suit_long_name = ['', 'Heart', 'Club', 'Diamond', 'Spade']
 
 var suit: int
 var value: int
@@ -19,6 +19,9 @@ func _init(starting_suit:int = Suits.NONE, starting_value:int = Values.NONE):
 	
 func getResourcePath():
 	return "res://res/"+str(suit_long_name[suit])+"/"+str(pv[value])+".png"
+
+func getRegion():
+	return Rect2(((value)*35), ((suit-1)*47), 35, 47)
 
 func hilo_value():
 	return hilo[value]
--- sims/flipthrough/flipthrough.gd
+++ sims/flipthrough/flipthrough.gd
@@ -4,17 +4,20 @@ enum FTStates {TIMER_INPUT, FLIPPING, ASKING, RESPONSE}
 
 # Static after intialization 
 var timer_limit = 1000
-var card_sprite
-var timer
-var linedit
+var card_sprite: Sprite
+var label: Label
+var timer: Timer
+var linedit: LineEdit
 
 # Dynamic values
 var state = FTStates.TIMER_INPUT
 var showing = Card.new(Card.Suits.SPADE, Card.Values.ACE)
 var deck = []
+var taken: Card
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	label = get_node("Label") as Label
 	linedit = get_node("LineEdit") as LineEdit
 	card_sprite = get_node("card") as Sprite
 	linedit.connect("text_entered", self, "input")
@@ -27,12 +30,23 @@ func _ready():
 	randomize()
 	deck.shuffle()
 	
+	taken = deck.pop_front()
+	
+	label.text = "How fast should we flip through the deck?\n(Input is in card/second)"
+	var atlas = card_sprite.texture as AtlasTexture
+	atlas.set_region(Rect2(0, 0, 35, 47))
+	
 func input(inp):
 	if state == FTStates.TIMER_INPUT:
 		start_timer(float(inp))
 		state = FTStates.FLIPPING
+		linedit.text = ""
+		label.text = ""
 	
 	if state == FTStates.ASKING:
+		#TODO get response and prompt if correct
+		state = FTStates.RESPONSE
+		label.text = "The correct count was "+str(taken.hilo_value()*-1)
 		pass
 
 func start_timer(delay):
@@ -46,20 +60,10 @@ func start_timer(delay):
 # Takes a card off the top of the deck and shows it.
 func flip():
 	showing = deck.pop_front() as Card
-	var texture = load(showing.getResourcePath())
-	card_sprite.texture = texture
+	var atlas = card_sprite.texture as AtlasTexture
+	atlas.set_region(showing.getRegion())
 	#print(deck)
 	if deck.empty():
+		state = FTStates.ASKING
+		label.text = "What is the count?"
 		timer.stop()
-
-var previous_state = null
-func _process(delta):
-	match(state):
-		FTStates.TIMER_INPUT:
-			pass
-		FTStates.FLIPPING:
-			pass
-		FTStates.ASKING:
-			pass
-		FTStates.RESPONSE:
-			pass
